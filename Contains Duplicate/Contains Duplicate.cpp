b5682be05fed5156a66696ca4302c1a819d6dc64
Medan <medan.dev@ya.ru>
2023-08-23 09:58:07 +0400

    Добавьте файлы проекта.

---
+++ Contains Duplicate/Contains Duplicate.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool static containsDuplicate(vector<int>& nums) 
+    {
+        unordered_set<int> numbers;
+
+        for (auto x : nums)
+        {
+            if (numbers.count(x) != 0) return true;
+            numbers.insert(x);
+        }
+
+        return false;
+    }
+};
+
+
+int main()
+{
+    vector<int> dataset { 1, 1, 3, 4, 7, 8, 9, 3 };
+
+    Solution::containsDuplicate(dataset);
+
+    return 0;
+}
+
+
---
+++ Valid Anagram/Valid Anagram.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> mapS, mapT;
+        int size = s.length();
+
+        if (size != t.length()) return false;
+
+        for (int i = 0; i < size; i++)
+        {
+            mapS[s[i]]++;
+            mapT[t[i]]++;
+        }
+
+        for (auto& it : mapS) 
+            if (mapT[it.first] != it.second) return false;
+
+        return true;
+    }
+};
+
+
+int main()
+{
+        
+    
+}
+
+
